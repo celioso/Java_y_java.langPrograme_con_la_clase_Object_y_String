e3b61d99cc032f5662d1c64c45921e12a4532ba8
Mario Celis <celioso1@hotmail.com>
2023-08-12 22:08:46 -0500

    java 12/08/2023

---
+++ src/co/com/bytebank/test/TestLibreria.java
@@ -0,0 +1,11 @@
+package co.com.bytebank.test;
+
+import co.com.bytebank.modelo.Cuenta;
+import co.com.bytebank.modelo.CuentaCorriente;
+
+public class TestLibreria {
+	public static void main(String[] args) {
+		CuentaCorriente cuenta = new CuentaCorriente(12, 12);
+		cuenta.deposita(200.0);
+	}
+}
